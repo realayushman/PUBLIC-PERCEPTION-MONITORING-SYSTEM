bdb6a6d3110de4871ac84d8c5a16a6afaee311b0
realayushman <humongous000001@gmail.com>
2026-01-21 10:15:45 +0530

    new api

--- flask_app/app.py
+++ flask_app/app.py
@@ -115,14 +115,9 @@ def predict():
         
         # Transform comments using the vectorizer
         transformed_comments = vectorizer.transform(preprocessed_comments)
-        
-        df = pd.DataFrame(
-             transformed_comments.toarray(),
-             columns=vectorizer.get_feature_names_out()
-        )
 
         # Make predictions
-        predictions = model.predict(df).tolist()  # Convert to list
+        predictions = model.predict(transformed_comments).tolist()  # Convert to list
         
         # Convert predictions to strings for consistency
         predictions = [str(pred) for pred in predictions]
